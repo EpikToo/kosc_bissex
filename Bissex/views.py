facaa4da474f6edc68fc0fccfafa26050da6224b
Florian Savalle <florian.savalle@covage.com>
2022-12-19 16:45:27 +0100

    Ajout des vues et des urls

--- Bissex/views.py
+++ Bissex/views.py
@@ -1,3 +1,22 @@
 from django.shortcuts import render
+from django.http import HttpResponse, JsonResponse
+from django.views.decorators.csrf import csrf_exempt
+from rest_framework.parsers import JSONParser
+from Bissex.models import Bissex
+from Bissex.serializers import BissexSerializer
 
-# Create your views here.
+@csrf_exempt
+#Historique des commandes
+def Bissex_history(request):
+    if request.method == 'GET':
+        Bissex = Bissex.objects.all()
+        serializer = BissexSerializer(Bissex, many=True)
+        return JsonResponse(serializer.data, safe=False)
+
+    elif request.method == 'POST':
+        data = JSONParser().parse(request)
+        serializer = BissexSerializer(data=data)
+        if serializer.is_valid():
+            serializer.save()
+            return JsonResponse(serializer.data, status=201)
+        return JsonResponse(serializer.errors, status=400)
