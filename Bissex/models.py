3a82356cde630020abd60e9c0bfa1856eee56cd6
Florian Savalle <florian.savalle@covage.com>
2022-12-19 16:09:51 +0100

    Correction des modèles

--- Bissex/models.py
+++ Bissex/models.py
@@ -1,10 +1,10 @@
 from django.db import models
 
 class Bissex_history(models.Model):
-    command_type = models.CharField
-    command_entry = models.CharField
-    command_result = models.CharField
-    command_date = models.CharField
+    command_type = models.CharField(max_length=100, blank=True, default='')
+    command_entry = models.CharField(max_length=100, blank=True, default='')
+    command_result = models.CharField(max_length=100, blank=True, default='')
+    command_date = models.CharField(max_length=100, blank=True, default='')
 
     class Meta:
         ordering = ['command_date']
