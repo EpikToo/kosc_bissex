75e6908f0456c43d56b5a7508bb971b4f3eb8f8d
Florian Savalle <florian.savalle@covage.com>
2022-12-19 15:07:38 +0100

    Création des modèles, du fichier serializers et modifications annexes

--- Bissex/models.py
+++ Bissex/models.py
@@ -1,3 +1,7 @@
 from django.db import models
 
-# Create your models here.
+class Bissex_history(models.Model):
+    command_type = models.CharField
+    command_entry = models.CharField
+    command_result = models.CharField
+    command_date = models.DateTimeField
