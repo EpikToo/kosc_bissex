71444aaad9dc313e7132005a9e1e4a572cf133b1
Florian Savalle <florian.savalle@covage.com>
2022-12-19 16:17:50 +0100

    Changement sur les serializers

--- Bissex/models.py
+++ Bissex/models.py
@@ -1,6 +1,6 @@
 from django.db import models
 
-class Bissex_history(models.Model):
+class Bissex(models.Model):
     command_type = models.CharField(max_length=100, blank=True, default='')
     command_entry = models.CharField(max_length=100, blank=True, default='')
     command_result = models.CharField(max_length=100, blank=True, default='')
--- Bissex/serializers.py
+++ Bissex/serializers.py
@@ -1,20 +1,7 @@
 from rest_framework import serializers
-from Bissex.models import Bissex_history
+from Bissex.models import Bissex
 
-class BissexSerializer(serializers.Serializer):
-    id = serializers.IntegerField(read_only=True)
-    command_type = serializers.CharField(required=False)
-    command_entry = serializers.CharField(required=False)
-    command_result = serializers.CharField(required=False)
-    command_date = serializers.CharField(required=False)
-
-    def create(self, validated_data):
-        return Bissex_history.objects.create(**validated_data)
-    
-    def update(self, instance, validated_data):
-        instance.command_type = validated_data.get('command_type', instance.command_type)
-        instance.command_entry = validated_data.get('command_entry', instance.command_entry)
-        instance.command_result = validated_data.get('command_result', instance.command_result)
-        instance.command_date = validated_data.get('command_date', instance.command_date)
-        instance.save()
-        return instance
+class SnippetSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Bissex
+        fields = ['id', 'command_type', 'command_entry', 'command_result', 'command_date']
