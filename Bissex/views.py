843cf859307c96b069bc80c8da4490da59f2b48c
Florian Savalle <florian.savalle@covage.com>
2022-12-20 13:29:41 +0100

    Changements sur les vues et templates

--- Bissex/views.py
+++ Bissex/views.py
@@ -5,20 +5,46 @@ from django.views.decorators.csrf import csrf_exempt
 from rest_framework.parsers import JSONParser
 from Bissex.models import Bissex
 from Bissex.serializers import BissexSerializer
-
+import re
 
 
 @csrf_exempt
-#Bssextile sur année (Endpoint 1)
+#Bissextile sur année (Endpoint 1)
 def Bissex_annee(request):
     template = loader.get_template('Bissex/Bissex_annee.html')
-    return HttpResponse(template.render(request))
+    
+    #Récupération de la date
+    txtbx = request.POST['year']
+    date = str(txtbx)
 
+    #1. On regarde si la textbox à été remplie
+    if date == "":
+        result = {"bissex": "Renseignez une date (yyyy):"}
+        return HttpResponse(template.render(result))
+
+    #2. On regarde si le format est valide
+    elif re.search('[a-zA-Z]', date) or re.search('[@_!#$%^&*()<>?/|}{~:]', date):
+        result = {"bissex": "Merci de renseigner une date valide."}
+        return HttpResponse(template.render(result))
+
+    #3. Sinon c'est valide
+    else:
+        #Check si l'année est bissextile
+        dateint = int(date)
+        if (((dateint % 4) == 0 and (dateint % 100) != 0) or ((dateint % 400) == 0)):
+            result = {"bissex": date + " est une année bissextile."}
+        else:
+            result = {"bissex": txtbx + " n'est pas une année bissextile."}
+
+        #Envoi du résultat vers le front
+        return HttpResponse(template.render(result))
 
 @csrf_exempt
 #Bissextiles sur range (Endpoint 2)
 def Bissex_range(request):
-    yo = "o"
+    template = loader.get_template('Bissex/Bissex_range.html')
+    return HttpResponse(template.render())
+
 
 @csrf_exempt
 #Historique des commandes (Endpoint 3)
