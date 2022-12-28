672c6982dcfca9f2e5b56db0e463feb5c109219d
Florian Savalle <florian.savalle@covage.com>
2022-12-28 10:43:45 +0100

    History récupéré sur le front

--- bissex_front/src/app/bissex-history/bissex-history.component.ts
+++ bissex_front/src/app/bissex-history/bissex-history.component.ts
@@ -10,24 +10,12 @@ import { take } from 'rxjs/operators';
 })
 export class BissexHistoryComponent {
   history! : string;
-  command_type!: string;
-  command_entry!: string;
-  command_result!: string;
-  command_date!: Date;
-  command_error!: string;
   constructor(private http: HttpClient) {}
 
   ngOnInit() {
     this.onClickHistory()
     this.history = ""
-    this.command_type = "test type";
-    this.command_entry = "test entry";
-    this.command_result = "test result";
-    this.command_date = new Date();
-    this.command_error = "test error";
   }
-
-  
 
   onClickHistory() {
     this.http.get('http://127.0.0.1:8000/bissex_history')
@@ -35,7 +23,12 @@ export class BissexHistoryComponent {
     .subscribe(response => {
       const keys = Object.keys(response) as Array<keyof typeof response>;
       for(var i in keys){
-        this.history += keys[i] + '<br/>';
+        this.history += keys[i] + ": ";
+        var nested  = Object.keys(response[keys[i]]) as Array<keyof typeof response>;
+        for(var u in nested){
+          this.history += " " + response[keys[i]][nested[u]];
+        }
+        this.history += '<br/>'
       }
     });
   }
