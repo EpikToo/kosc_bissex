e46397742c6eaf0f5ab2e1c0fb3c62170bae8def
Florian Savalle <florian.savalle@covage.com>
2022-12-28 11:49:56 +0100

    Update des routes

--- bissex_front/src/app/app-routing.module.ts
+++ bissex_front/src/app/app-routing.module.ts
@@ -1,11 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { BissexHistoryComponent } from './bissex-history/bissex-history.component';
+import { BissexIndexComponent } from './bissex-index/bissex-index.component';
 import { BissexRangeComponent } from './bissex-range/bissex-range.component';
 import { BissexYearComponent } from './bissex-year/bissex-year.component';
 
 
 const routes: Routes = [
+    { path: '', component: BissexIndexComponent },
     { path: 'history', component: BissexHistoryComponent },
     { path: 'year', component: BissexYearComponent },
     { path: 'range', component: BissexRangeComponent },
