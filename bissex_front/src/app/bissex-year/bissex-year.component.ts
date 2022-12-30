c7c6ca0947f44297bf29e30caccb75239caeb06b
Florian Savalle <florian.savalle@covage.com>
2022-12-30 14:13:00 +0100

    Style et logique bissex year done.

--- bissex_front/src/app/bissex-year/bissex-year.component.ts
+++ bissex_front/src/app/bissex-year/bissex-year.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { take } from 'rxjs/operators';
 
 @Component({
   selector: 'app-bissex-year',
@@ -6,5 +8,31 @@ import { Component } from '@angular/core';
   styleUrls: ['./bissex-year.component.scss']
 })
 export class BissexYearComponent {
+  result! : string;
+  constructor(private http: HttpClient) {}
 
+  ngOnInit() {
+    this.result = "..."
+  }
+
+  onSubmit() {
+    const tbx = document.getElementById("yeartbx") as HTMLInputElement | null;
+    this.http.get('http://127.0.0.1:8000/bissex_annee/?year='+tbx?.value)
+    .pipe(take(1))
+    .subscribe(response => {
+      const keys = Object.keys(response) as Array<keyof typeof response>;
+      if (String(response[keys[3]]) == "true")
+      {
+        this.result = tbx?.value + " est une année bissextile !";
+      }
+      else if (String(response[keys[3]]) == "false")
+      {
+        this.result = tbx?.value + " n'est pas une année bissextile.";
+      }
+      else if (String(response[keys[3]]) == "null")
+      {
+        this.result = String(response[keys[4]]);
+      }
+    });
+  }
 }
