85bfd549da239444d0493f3648a6b778f7480ad9
Florian Savalle <florian.savalle@covage.com>
2023-01-03 14:00:36 +0100

    Correction module

--- bissex_front/src/app/app.module.ts
+++ bissex_front/src/app/app.module.ts
@@ -1,9 +1,7 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppComponent } from './app.component';
 import { BissexHistoryComponent } from './bissex-history/bissex-history.component';
-
 import { HttpClientModule } from '@angular/common/http';
 import { BissexYearComponent } from './bissex-year/bissex-year.component';
 import { BissexRangeComponent } from './bissex-range/bissex-range.component';
