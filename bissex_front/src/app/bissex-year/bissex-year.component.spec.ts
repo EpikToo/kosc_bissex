830ff4eb9cf5c31679776f26f81002b01528324e
Florian Savalle <florian.savalle@covage.com>
2023-01-03 17:15:33 +0100

    Début tests unitaires front (bissex_year)

--- bissex_front/src/app/bissex-year/bissex-year.component.spec.ts
+++ bissex_front/src/app/bissex-year/bissex-year.component.spec.ts
@@ -1,16 +1,16 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
-
 import { BissexYearComponent } from './bissex-year.component';
+import { By } from '@angular/platform-browser';
 
 describe('BissexYearComponent', () => {
   let component: BissexYearComponent;
   let fixture: ComponentFixture<BissexYearComponent>;
-  
+
 
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [ BissexYearComponent ]
+      declarations: [ BissexYearComponent ],
     })
     .compileComponents();
 
@@ -21,5 +21,9 @@ describe('BissexYearComponent', () => {
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+  it('should print the http response correctly', () => {
+    const divresult = fixture.debugElement.query(By.css('.divresult'));
+    expect(String(divresult)).toBe("salut")
   });
 });
